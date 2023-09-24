3e583736a32b97daed6f0998fa5471ea8066a75d
noordibou <noor.dibou@hotmail.com>
2023-09-24 16:17:23 -0500

    test

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,10 @@ export default function About() {
   return (
     <section
       className='relative max-w-contentContainer mx-auto top-36 md:top-60 py-20 lgl:py-32 mdl:py-24 flex flex-col gap-20  '>
-      <div>
+      <motion.div
+      initial={{ y: -10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.6, delay: 0.8 }}>
         < Section title="About" />
         <div className='flex flex-col lgl:flex-row gap-10 '>
           <div>
@@ -30,7 +33,7 @@ export default function About() {
             </p>
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
